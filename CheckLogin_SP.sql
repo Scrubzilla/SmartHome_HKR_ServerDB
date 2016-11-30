62cf035d45a5712d317d47bec2f323f3b08c4837
Jocke90 <roslund1990@gmail.com>
2016-11-30 22:48:07 +0100

    Minor change to CheckLogin SP
    Added so the user_id is fetched. Think it is correct now.

--- CheckLogin_SP.sql
+++ CheckLogin_SP.sql
@@ -6,7 +6,8 @@
 
 CREATE PROCEDURE CheckLogin
   @username varchar(24) = null,
-  @password varchar(32) = null
+  @password varchar(32) = null,
+  @user_id int OUTPUT
 
 AS
 
@@ -14,6 +15,8 @@ AS
   IF EXISTS(SELECT * FROM [users] WHERE username = @username AND user_password = @password)
 
     BEGIN
+    -- Get user_id that is attached to the correct user.
+    SELECT @user_id = User_id FROM [users] WHERE username = @username AND user_password = @password
 
     RETURN 0
 
